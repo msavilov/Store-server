242be71c2b10e94f32360a3d49035991a0c4af76
Maksim Savilov <m.savilov@yandex.ru>
2023-05-12 21:18:55 +0300

    add Store-server project

---
+++ store/users/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView
+
+from common.views import TitleMixin
+from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
+from users.models import EmailVerification, User
+
+
+class UserLoginView(TitleMixin, LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    title = 'Store - Авторизация'
+
+
+class UserRegistrationView(SuccessMessageMixin, TitleMixin, CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    title = 'Store - Регистрация'
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+    success_message = "Поздравляем! Вы успешно зарегистрировались"
+
+
+class UserProfileView(TitleMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    title = 'Store - Личный кабинет'
+
+    def get_success_url(self):
+        # object - это model
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+
+class EmailVerificationView(TitleMixin, TemplateView):
+    title = 'Store - Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
+
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verification = EmailVerification.objects.filter(
+            user=user,
+            code=code,
+        )
+        if (email_verification.exists()
+                and not email_verification.last().is_expired()):
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(
+                request,
+                *args,
+                **kwargs,
+            )
+        else:
+            return HttpResponseRedirect(reverse('index'))
